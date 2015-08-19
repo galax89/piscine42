2566ea8875e11a49cfa64c7c96dc5a449f151b35
galax89 <galax89@gmail.com>
2015-08-19 13:40:24 +0200

    Update ft_iterative_factorial.c
    fn + main

--- j04/ex00/ft_iterative_factorial.c
+++ j04/ex00/ft_iterative_factorial.c
@@ -18,25 +18,21 @@ int		ft_iterative_factorial(int nb)
 	fac = nb;
 
 	if (nb < 0)
-	{
-		return (0);
-	}
+		return (0); //on travaille uniquement sur des entiers donc on vire les nb negatifs
 
-	else if (nb >1)
+	if (nb = 1)
+		return (1);
+	
+	if (nb > 1)
 	{
 		nb--;
 		fac *= nb;
-		return (0);
 	}
-
-	else  
-	{
-		return (fac);
-	}
+	return (fac);
 }
-
+// évidemment le main n'est pas à rendre dans le repertoire (abrégé repo pour la suite)
 int 	main(void)
 {
-	ft_iterative_factorial(10);
+	ft_iterative_factorial(10); //on test avec nb = 10
 	return (0);
 }
